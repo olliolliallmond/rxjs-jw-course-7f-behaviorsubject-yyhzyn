b0d6557b83599af23d86b9618a5d1ba6331496e2
olliolliallmond <aallmondcar@gmail.com>
2021-08-10 12:53:33 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# rxjs-jw-course-7f-behaviorsubject-yyhzyn
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/rxjs-jw-course-7f-behaviorsubject-yyhzyn)
---
+++ index.ts
@@ -0,0 +1,31 @@
+import { fromEvent, Subject, BehaviorSubject } from 'rxjs';
+import { withLatestFrom } from 'rxjs/operators';
+
+const loggedInSpan: HTMLElement = document.querySelector('span#logged-in');
+const loginButton: HTMLElement = document.querySelector('button#login');
+const logoutButton: HTMLElement = document.querySelector('button#logout');
+const printStateButton: HTMLElement = document.querySelector(
+  'button#print-state'
+);
+
+const isLoggedIn$ = new BehaviorSubject<boolean>(false);
+
+fromEvent(loginButton, 'click').subscribe(() => isLoggedIn$.next(true));
+fromEvent(logoutButton, 'click').subscribe(() => isLoggedIn$.next(false));
+
+//Navigation bar
+isLoggedIn$.subscribe(
+  isLoggedIn => (loggedInSpan.innerText = isLoggedIn.toString())
+);
+
+// Buttons
+isLoggedIn$.subscribe(isLoggedIn => {
+  logoutButton.style.display = isLoggedIn ? 'block' : 'none';
+  loginButton.style.display = isLoggedIn ? 'none' : 'block';
+});
+
+fromEvent(printStateButton, 'click')
+  .pipe(withLatestFrom(isLoggedIn$))
+  .subscribe(([event, isLoggedIn]) =>
+    console.log('User logged in? ', isLoggedIn)
+  );
